85caa24a8435f9dad1e63e1bb90762c8371708af
Kwonyul <kwonyul@amazon.com>
2021-05-25 02:42:55 +0900

    Refactor infra codes

--- bin/stack/serverless-stack.ts
+++ bin/stack/serverless-stack.ts
@@ -13,13 +13,13 @@ export class ServerlessStack extends cdk.Stack {
         const table = this.createDdbTable();
         
         const topic = this.createSnsTopic();
-        const topicFunction = this.createLambdaFunctionTopic(table, topic);
-
-        const restFunction = this.createLambdaFunctionRest(table);
-        const rest = this.createApiRest(restFunction);
+        this.createLambdaFunctionTopic(table, topic);
 
         const bucket = this.createS3Bucket();
-        const bucketFunction = this.createLambdaFunctionBucket(table, bucket);
+        this.createLambdaFunctionBucket(table, bucket);
+
+        const restFunction = this.createLambdaFunctionRest(table);
+        this.createApiRest(restFunction);
     }
 
     private createDdbTable(): ddb.Table {
@@ -61,46 +61,6 @@ export class ServerlessStack extends cdk.Stack {
         return func;
     }
 
-    private createApiRest(apiFunction: lambda.Function) {
-        const rest = new api.LambdaRestApi(this, 'rest-api', {
-            restApiName: `${this.stackName}-APIs`,
-            handler: apiFunction,
-            proxy: false,
-            deployOptions: {
-                loggingLevel: api.MethodLoggingLevel.INFO
-            }
-        });
-
-        const resourceName = 'books';
-        const books = rest.root.addResource(resourceName);
-        books.addMethod('GET');
-
-        new cdk.CfnOutput(this, 'api-endpoint', {
-            value: `https://${rest.restApiId}.execute-api.${this.region}.amazonaws.com/prod/${resourceName}`,
-            exportName: 'APIGatewayEndpoint'
-        })
-
-        return rest;
-    }
-
-    private createLambdaFunctionRest(table: ddb.Table): lambda.Function {
-        const path = 'codes/lambda/rest-function/src';
-
-        const func = new lambda.Function(this, 'rest-function', {
-            functionName: `${this.stackName}-rest-function`,
-            runtime: lambda.Runtime.PYTHON_3_6,
-            handler: 'handler.handle',
-            code: lambda.Code.fromAsset(path),
-            environment: {
-                "TABLE_NAME": table.tableName
-            }
-        });
-
-        table.grantReadData(func);
-
-        return func;
-    }
-
     private createS3Bucket(): s3.Bucket {
         return new s3.Bucket(this, 'bucket', {
             bucketName: `${this.stackName.toLowerCase()}-${this.region}-${this.account.substring(0, 5)}`
@@ -130,5 +90,45 @@ export class ServerlessStack extends cdk.Stack {
         bucket.grantRead(func);
 
         return func;
+    }
+
+    private createLambdaFunctionRest(table: ddb.Table): lambda.Function {
+        const path = 'codes/lambda/rest-function/src';
+
+        const func = new lambda.Function(this, 'rest-function', {
+            functionName: `${this.stackName}-rest-function`,
+            runtime: lambda.Runtime.PYTHON_3_6,
+            handler: 'handler.handle',
+            code: lambda.Code.fromAsset(path),
+            environment: {
+                "TABLE_NAME": table.tableName
+            }
+        });
+
+        table.grantReadData(func);
+
+        return func;
+    }
+
+    private createApiRest(apiFunction: lambda.Function) {
+        const rest = new api.LambdaRestApi(this, 'rest-api', {
+            restApiName: `${this.stackName}-APIs`,
+            handler: apiFunction,
+            proxy: false,
+            deployOptions: {
+                loggingLevel: api.MethodLoggingLevel.INFO
+            }
+        });
+
+        const resourceName = 'books';
+        const books = rest.root.addResource(resourceName);
+        books.addMethod('GET');
+
+        new cdk.CfnOutput(this, 'api-endpoint', {
+            value: `https://${rest.restApiId}.execute-api.${this.region}.amazonaws.com/prod/${resourceName}`,
+            exportName: 'APIGatewayEndpoint'
+        })
+
+        return rest;
     }
 }
