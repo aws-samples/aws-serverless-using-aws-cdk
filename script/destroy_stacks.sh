2ab9286d9d31b8a81f166f374e75d6e61d0789e2
Kwonyul <kwonyul@amazon.com>
2022-08-16 18:53:31 +0900

    Update: update script files

--- script/destroy_stacks.sh
+++ script/destroy_stacks.sh
@@ -6,6 +6,10 @@ CONFIG_INFRA=config/app-config-demo.json
 ACCOUNT=$(cat $CONFIG_INFRA | jq -r '.Project.Account') #ex> 123456789123
 REGION=$(cat $CONFIG_INFRA | jq -r '.Project.Region') #ex> us-east-2
 PROFILE_NAME=$(cat $CONFIG_INFRA | jq -r '.Project.Profile') #ex> cdk-demo
+PROJECT_NAME=$(cat $CONFIG_INFRA | jq -r '.Project.Name') #ex> ServerlessCdk
+PROJECT_STAGE=$(cat $CONFIG_INFRA | jq -r '.Project.Stage') #ex> Demo
+PROJECT_PREFIX=$PROJECT_NAME$PROJECT_STAGE #ex> ServerlessCdkDemo
+PREFIX_LOW=$(echo $PROJECT_PREFIX | tr '[:upper:]' '[:lower:]')
 
 echo ==--------ConfigInfo---------==
 echo $CONFIG_INFRA
@@ -31,7 +35,7 @@ echo .
 echo .
 
 echo ==--------DeleteResourcesManually---------==
-aws s3 rb s3://serverlesscdkdemo-serverlessstack-${REGION}-${ACCOUNT:0:5} --force --profile $PROFILE_NAME
-aws dynamodb delete-table --table-name ServerlessCdkDemo-ServerlessStack-ddb-table --profile $PROFILE_NAME
+aws s3 rb s3://${PREFIX_LOW}-serverlessstack-${REGION}-${ACCOUNT:0:5} --force --profile $PROFILE_NAME
+aws dynamodb delete-table --table-name ${PROJECT_PREFIX}-ServerlessStack-ddb-table --profile $PROFILE_NAME
 echo .
 echo .
--- script/upload_s3.sh
+++ script/upload_s3.sh
@@ -6,9 +6,9 @@ CONFIG_INFRA=config/app-config-demo.json
 ACCOUNT=$(cat $CONFIG_INFRA | jq -r '.Project.Account') #ex> 123456789123
 REGION=$(cat $CONFIG_INFRA | jq -r '.Project.Region') #ex> us-east-2
 PROFILE_NAME=$(cat $CONFIG_INFRA | jq -r '.Project.Profile') #ex> cdk-demo
-PROJECT_NAME=$(cat $CONFIG_INFRA | jq -r '.Project.Name') #ex> TextClassification
-PROJECT_STAGE=$(cat $CONFIG_INFRA | jq -r '.Project.Stage') #ex> Dev
-PROJECT_PREFIX=$PROJECT_NAME$PROJECT_STAGE #ex> TextClassificationDev
-PREFIX=$(echo $PROJECT_PREFIX | tr '[:upper:]' '[:lower:]')
+PROJECT_NAME=$(cat $CONFIG_INFRA | jq -r '.Project.Name') #ex> ServerlessCdk
+PROJECT_STAGE=$(cat $CONFIG_INFRA | jq -r '.Project.Stage') #ex> Demo
+PROJECT_PREFIX=$PROJECT_NAME$PROJECT_STAGE #ex> ServerlessCdkDemo
+PREFIX_LOW=$(echo $PROJECT_PREFIX | tr '[:upper:]' '[:lower:]')
 
-aws s3 cp script/input_s3.json s3://"$PREFIX"-serverlessstack-"$REGION"-"${ACCOUNT:0:5}"/batch/input_s3.json --profile $PROFILE_NAME
+aws s3 cp script/input_s3.json s3://"$PREFIX_LOW"-serverlessstack-"$REGION"-"${ACCOUNT:0:5}"/batch/input_s3.json --profile $PROFILE_NAME
